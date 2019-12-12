ff9f3e3bacb23842b5ec8e9ccaff44b201f470ec
JC Smiley <jcsmileyjr@gmail.com>
2019-12-11 23:15:59 -0600

    Updated Sample Milestone screen with Checkbox
    - included sample goal.json for real data use. Failed

--- screens/MilestonesScreen.js
+++ screens/MilestonesScreen.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {View, Stylesheet} from 'react-native';
-import { Container, Text, Content, Button, Icon, Title, Card, CardItem } from 'native-base';
+import {View, StyleSheet} from 'react-native';
+import { Container, Text, Content, Button, CheckBox, Icon, Title, Card, CardItem, Body } from 'native-base';
 import { AppLoading } from 'expo';
 
+import {userGoals} from '../data/goals.json';
 import Head from '../components/header.js';
 import Foot from '../components/Foot.js';
 
 export default class Milestones extends React.Component {
-    state = { isReady: false };
+    state = { isReady: false};
     async componentWillMount() {
       await Expo.Font.loadAsync({
         'Roboto': require('../node_modules/native-base/Fonts/Roboto.ttf'),
@@ -25,10 +26,42 @@ export default class Milestones extends React.Component {
             <Container>
                 <Head />
                 <Content>
-                    <View><Text>HEllo World</Text></View>
+                    <View><Text style={styles.milestoneTitle}>Practice Building Apps</Text></View>
+                    <View>
+                        <Card transparent>
+                            <CardItem style={styles.milestoneStyle}>
+                                <CheckBox checked={false} style={styles.checkboxStyle} color='#9C08AB' />
+                                <Body>
+                                    <Text style={[styles.checkboxText]}>Build a kids 1,2,3 counting mobile app</Text>
+                                </Body>
+                            </CardItem>
+                        </Card>
+                    </View>
                 </Content>
                 <Foot />
             </Container>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    milestoneStyle:{
+        display:"flex", //Ensure the goal id and statement is in a row
+        flexDirection:"row",
+      },
+    checkboxText:{
+        color:'#9C08AB',  //signature purple color
+        fontSize:30,  //text size        
+      },
+    checkboxStyle:{
+        width: 40, //width of the checkbox
+        height: 40, //height of the checkbox
+        marginRight:20, //space between the index and statement
+      },
+      milestoneTitle: {
+          textAlign:"center",
+          fontSize:35,
+          fontWeight:"bold",  //Bigger text
+          color:'#9C08AB',  //signature purple color
+      }
+});
