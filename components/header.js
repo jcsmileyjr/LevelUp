6ad705e10a4cf31213fc2063077c854424b2ec2c
JC Smiley <jcsmileyjr@gmail.com>
2019-12-21 09:39:34 -0600

    WIP: Scaffold basic modal for home menu

--- components/header.js
+++ components/header.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TouchableNativeFeedback, Modal } from 'react-native';
 
-import { Container, Text, Header, Content, Left, Body, Right, Button, Icon} from 'native-base';
-
+import { Container, Text, Header, Content, Left, Body, Right, Button, Icon, Grid, Row, Col} from 'native-base';
+//onClick={() => setCount(count + 1)}
 const Head = (props) => {
+
+  const changeModalVisible = () => { setModalVisble(!isVisible)};
+  const [isVisible, setModalVisble] = useState(false);
     return(
         <View>
         {/*Displays the App's Title, current section, and menu button */}
@@ -11,11 +14,34 @@ const Head = (props) => {
                 <Left style={styles.headerContent}><Text style={styles.headerText}>Level Up</Text></Left>
                 <Body style={styles.headerContent, styles.headerSection}><Text style={[styles.headerText, styles.headerTitle]}>{props.title}</Text></Body>
                 <Right style={styles.headerContent}>
-                    <Button transparent>
+                    <Button transparent onPress={() => setModalVisble(true)}>
                         <Icon name='menu' />
                     </Button>
                 </Right>
-            </Header>            
+            </Header>
+            <Modal  animationType="slide" 
+                    visible={isVisible}
+                    transparent={true}>
+                <Container>
+                  <Grid>
+                    <Row>
+                      <Col><Text>1</Text></Col>
+                      <Col><Text>1</Text></Col>
+                      <Col><Text>1</Text></Col>
+                    </Row>
+                    <Row >
+                      <Col style={styles.buttonContainer}>
+                        <TouchableNativeFeedback onPress={() => setModalVisble(false) } >
+                          <View style={styles.buttonStyle}>
+                            <Text style={styles.buttonText}>Close</Text>
+                          </View>
+                        </TouchableNativeFeedback>
+                      </Col>
+                    </Row>
+                  </Grid>
+                </Container>
+              
+            </Modal>            
         </View>
     );
 }
@@ -47,4 +73,21 @@ const styles = StyleSheet.create({
         textDecorationLine:"underline", //Text is underlined
         fontWeight:"bold",  //Bigger text
       },
+      buttonStyle:{
+        backgroundColor:'navy',//signature purple color 
+        padding: 10, //space between button title and border
+        margin: 10, //whitespace between button and other elements
+        width: 250, //width of button
+        borderColor:'navy',//signature purple color
+        borderRadius: 15, //round the corners    
+      },
+      buttonContainer:{
+        alignItems:"center",  //help center the button
+        justifyContent:"center",
+      },
+      buttonText:{
+        color: "#ffffff", //text color
+        textAlign:"center", //center the text
+        fontWeight:"bold",  //Bigger text
+      }      
 });
