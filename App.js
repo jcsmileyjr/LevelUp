dac3aa3f116fe1c461fe55ec57f73da39e9b86ec
JC Smiley <jcsmileyjr@gmail.com>
2019-12-31 17:45:31 -0600

    Basic scaffold of onboarding

--- App.js
+++ App.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import {Root} from "native-base";
+import AppIntroSlider from 'react-native-app-intro-slider';
 
 import GoalScreen from './screens/GoalScreen.js';
 import MilestoneScreen from './screens/MilestonesScreen.js';
 import PlanningScreen from './screens/PlanningScreen.js';
 import AchievementScreen from './screens/AcheivementScreen.js';
 
-export default function App() {
+export default function App() { 
+  const [show_Main_App, setShow_Main_App] = useState(false);
 
-  return (
-    <Root>
-      <AppContainer />
-    </Root>
+  on_Done_all_slides = () => {
+    setShow_Main_App(true);
+  };
+  
+  on_Skip_slides = () => {
+    setShow_Main_App(true);
+  };
+
+  if(show_Main_App){
+    return (
+      <Root>
+        <AppContainer />
+      </Root>    
+    );
+  }else {
+    return (
+      <AppIntroSlider slides = {slides} onDone={this.on_Done_all_slides} showSkipButton={true} onSkip={this.on_Skip_slides} />
+    );
     
-  );
+  }
+  
 }
 
 const navigationOptions = {
@@ -35,4 +52,39 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titleStyle:{
+    fontSize:40,
+    fontWeight:"bold",
+    color:"white",
+  },
+  bodyStyle:{
+    fontSize:30,
+    color:"white",
+  }, 
+  image: { 
+   width: 200, 
+   height: 200, 
+   resizeMode: 'contain' 
+  }
 });
+
+const slides = [
+  {
+    key:"k1",
+    title:"Guide your Learning Path by planning Goals",
+    backgroundColor: '#2B65EC',
+    titleStyle: styles.titleStyle,
+  },
+  {
+    key:"k2",
+    title:"Break Goals Down Into Milestones to Transform them into your new Reality",
+    backgroundColor: '#00009C',    
+    titleStyle: styles.titleStyle,
+  },
+  {
+    key:"k3",
+    title:"A visually reminder of how much you have achieved and what is the next step toward your planned future",
+    backgroundColor:'#2B65EC',
+    titleStyle: styles.titleStyle,
+  }
+]
